439e1396ad667261dd97bcac3a43558d82d838e2
Hisano Sato <hisano@Hisanos-MacBook-Air.local>
2023-07-21 18:33:15 +1000

    add  http

--- src/Components/Service/API.js
+++ src/Components/Service/API.js
@@ -1,4 +1,4 @@
-const BASE = "https://api.weatherapi.com/v1/forecast.json?key=";
+const BASE = "http://api.weatherapi.com/v1/forecast.json?key=";
 const URL = (search) =>
   `${BASE}${process.env.REACT_APP_API_KEY}&q=${search}&days=7&aqi=no&alerts=no`;
 
